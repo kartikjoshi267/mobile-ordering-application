42e71a8350d26470420f411c78c3c4bdf1f1086d
Kartik <kartikjoshi267@gmail.com>
2023-12-23 19:13:36 +0530

    Fix mongo uri issue

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mobile_ordering');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.log(error);
